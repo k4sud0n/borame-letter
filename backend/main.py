1b7454b38d45b9b4b237c59d1865fc0dc65ea02c
Sihyun Kim <k4sud0n@gmail.com>
2022-01-30 08:15:36 +0000

    Add chromedriver

--- backend/main.py
+++ backend/main.py
@@ -2,14 +2,22 @@ from typing import Optional
 
 from fastapi import FastAPI
 
-from bot import news
+from bot import sender, crawler
 
 app = FastAPI()
 
+@app.get("/")
+def send_news():
+    return sender.get_news_title()
+
 @app.get("/news")
 def get_news():
-    return news.get_title()
+    return crawler.get_news_title()
+
+@app.get("/weather")
+def get_covid():
+    return crawler.get_today_weather()
 
 @app.get("/covid")
 def get_covid():
-    return news.get_covid_confirm_case()
+    return crawler.get_covid_confirm_case()
