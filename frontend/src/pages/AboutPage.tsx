9f8d938bc4f350efec2aa6096ba462caf6c71b5c
Su-Yong <simssy2205@gmail.com>
2022-02-06 07:21:53 +0000

    setup about page, fix package.json path

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,9 +1,17 @@
-import Header from '@/components/Header';
-
 const AboutPage = (): JSX.Element => {
   return (
-    <div className={'grow flex flex-col'}>
-      안녕
+    <div className={'grow md:container md:mx-auto flex flex-col py-12'}>
+      <div
+        className={`
+          flex flex-col justify-start items-start bg-slate-200 rounded-lg p-3 cursor-pointer transition-all
+          border-4 border-slate-200
+          hover:border-slate-300 active:bg-slate-300
+        `}
+      >
+        <div className={'text-lg font-bold'}>공지사항 제목</div>
+        <div className={'text-md text-slate-500 font-light'}>공지사항 내용 요약</div>
+        <div className={'text-sm self-end text-slate-500'}>2021. 11. 08.</div>
+      </div>
     </div>
   );
 };
