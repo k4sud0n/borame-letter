1075bee23f2c78695cad3c377a94b63b04f47581
Sihyun Kim <k4sud0n@gmail.com>
2022-02-13 05:59:16 +0000

    Add pipenv

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,25 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from database import User, session
 
 app = FastAPI()
 
+class NewUser(BaseModel):
+    name: str
+    cardinal_number: float
+    birth_year: float
+    birth_month: float
+    birth_date: float
+
+@app.get("/")
+def hi():
+    return 'hi'
+
 @app.post("/submit")
-async def submit():
-    session.
+async def submit(user: NewUser):
     # session.add(User('이창연', 834, 2002, 12, 31))
     # session.commit()
-    return 'success'
+    return user
