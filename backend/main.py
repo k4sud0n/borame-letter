5d1aafc55a25385dc87d4bdf9f6b0ac7c4f97aa4
Sihyun Kim <k4sud0n@gmail.com>
2022-02-20 22:39:26 +0900

    Add main api endpoint

--- backend/main.py
+++ backend/main.py
@@ -43,8 +43,39 @@ class NewReview(BaseModel):
     rating: int
 
 @app.get("/")
-def main():
-    return 'Hello, Borame Letter!'
+async def main():
+    every_news = open('./letters/every_news.txt', 'r').read()
+    political_news = open('./letters/political_news.txt', 'r').read()
+    world_news = open('./letters/world_news.txt', 'r').read()
+    entertain_news = open('./letters/entertain_news.txt', 'r').read()
+    esports_news = open('./letters/esports_news.txt', 'r').read()
+
+    korea_football = open('./letters/korea_football.txt', 'r').read()
+    world_football = open('./letters/world_football.txt', 'r').read()
+    korea_baseball = open('./letters/korea_baseball.txt', 'r').read()
+    world_baseball = open('./letters/world_baseball.txt', 'r').read()
+
+    today_weather = open('./letters/weather.txt', 'r').read()
+    covid_confirm_case = open('./letters/covid.txt', 'r').read()
+
+    total_users = session.query(User).count()
+
+    return {
+        'total_users': total_users, 
+        'letters': {
+            'every_news': every_news,
+            'political_news': political_news,
+            'world_news': world_news,
+            'entertain_news': entertain_news,
+            'esports_news': esports_news,
+            'korea_football': korea_football,
+            'world_football': world_football,
+            'korea_baseball': korea_baseball,
+            'world_baseball': world_baseball,
+            'today_weather': today_weather,
+            'covid_confirm_case': covid_confirm_case
+        }
+    }
 
 @app.post("/user")
 async def post_user(user: NewUser):
