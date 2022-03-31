4951c78d71909b3cec36e7c156b541893f93210a
Sihyun Kim <k4sud0n@gmail.com>
2022-03-31 12:13:23 +0900

    Update ErrorPage.tsx

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,11 +1,12 @@
 import { Link, useLocation } from 'wouter';
+import searchImage from '../../assets/image/search.png';
 
 const ErrorPage = (): JSX.Element => {
   const [location] = useLocation();
 
   return (
     <div className={'grow md:container flex flex-col justify-center items-center w-full p-5'}>
-      <img className={'w-36 h-36 md:w-72 md:h-72'} src={'../../assets/image/search.png'} />
+      <img className={'w-36 h-36 md:w-72 md:h-72'} src={searchImage} />
       <div className={'flex flex-row'}>
         <Link className={'text-sky-500'} href={window.location.origin}>
           {window.location.origin}
