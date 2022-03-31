9c574b2c59664379689f91ad50fe83006dd8a1eb
Sihyun Kim <k4sud0n@gmail.com>
2022-03-31 10:37:32 +0900

    Fix image link

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -5,7 +5,7 @@ const ErrorPage = (): JSX.Element => {
 
   return (
     <div className={'grow md:container flex flex-col justify-center items-center w-full p-5'}>
-      <img className={'w-36 h-36 md:w-72 md:h-72'} src={'../../assets/image/search.png'} />
+      <img className={'w-36 h-36 md:w-72 md:h-72'} src={require('../../assets/image/search.png')} />
       <div className={'flex flex-row'}>
         <Link className={'text-sky-500'} href={window.location.origin}>
           {window.location.origin}
